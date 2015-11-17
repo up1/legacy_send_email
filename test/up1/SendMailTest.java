767a72d30ccf08d2a46ef131728f2d51d5c927f1
somkiat <somkiat.p@gmail.com>
2015-11-17 20:07:21 +0700

    Add first test and found first dependency is Session object

---
+++ test/up1/SendMailTest.java
@@ -0,0 +1,15 @@
+package up1;
+
+
+import org.junit.Test;
+
+public class SendMailTest {
+
+    @Test
+    public void send_mail() {
+        Main main = new Main();
+        main.sendEmail();
+    }
+
+
+}
